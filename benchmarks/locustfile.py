63cc4ea59d63d602f5b9feb60c5bdd6a82d95980
Sam Stoelinga <sammiestoel@gmail.com>
2023-10-28 22:00:46 -0700

    use random sentence in benchmark

--- benchmarks/locustfile.py
+++ benchmarks/locustfile.py
@@ -1,11 +1,30 @@
+import random
+
+
 from locust import HttpUser, task
+
+
+def random_sentence() -> str:
+    subjects = ["I", "You", "Bob", "Sue", "We", "Sam", "Nick", "Bob"]
+    verbs = ["eat", "make", "read", "write", "create", "code"]
+    objects = [
+        "a book",
+        "the homework",
+        "a poem",
+        "a song",
+        "an email",
+        "some software",
+    ]
+
+    return f"{random.choice(subjects)} {random.choice(verbs)} {random.choice(objects)}."
 
 
 class EmbeddingUser(HttpUser):
     @task
     def get_embeddings(self):
+        sentence = random_sentence()
         embedding_request = {
-            "input": "substratus.ai has some great LLM OSS projects for K8s",
+            "input": sentence,
             "model": "ignored",
         }
         self.client.post("/v1/embeddings", json=embedding_request)
